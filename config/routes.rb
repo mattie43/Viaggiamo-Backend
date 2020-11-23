dec693f28b7666ae4d87468d43a14ce421583931
slurio <samantha.lurio@gmail.com>
2020-11-23 12:28:21 -0500

    created production branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
   resources :messages
   resources :categories
   resources :users
 
-  get "/lessons/:language", to: "lessons#index"
+  get "/lessons/:language", to: "lessons#index" 
   get "/endless/:language", to: "endless#index"
   
 end
